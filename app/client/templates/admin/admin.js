d56c46e9a4d2cacb4b323283005da116e673d7af
Basiczombie <issacgable@gmail.com>
2016-04-25 12:49:41 -0600

    Refactored admin logic

--- app/client/templates/admin/admin.js
+++ app/client/templates/admin/admin.js
@@ -10,31 +10,33 @@ Template.Admin.events({
 /*****************************************************************************/
 Template.Admin.helpers({
   users: function() {
-    return Meteor.users.find();
-  },
-  score: function() {
-    var user = this._id;
-    var record = Score.find({team_id:user}).fetch();
-    return record.reduce(function(previous, current){
-      return previous+current.score;
-    }, 0);
-  },
-  questionsDone: function() {
-    var user = this._id;
-    var record = Score.find({team_id:user}).fetch();
-    return record.length;
-  },
-  timeIn: function() {
-    var user = this._id;
-    var record = Score.find({team_id:user}, {sort: {submitted: -1}}).fetch();
-    return record[0].submitted;
+    var userRecord = Meteor.users.find();
+    var scoreRecord = Score.find().fetch();
+    var newUserRecord = userRecord.map(currUserRecord => {
+      var user_score = scoreRecord.filter(score => currUserRecord._id === score.team_id);
+      if (user_score.length) {
+        user_score = user_score.sort((a, b) => {
+          return new Date(b.submitted) - new Date(a.submitted);
+        });
+        currUserRecord.score = user_score.reduce(function(previous, current){
+          return previous+current.score;
+        }, 0);
+        currUserRecord.questionsDone = user_score.length;
+        currUserRecord.timeIn = user_score[0].submitted;
+        currUserRecord.latestScoreId = user_score[0]._id;
+      }
+      return currUserRecord;
+    });
+    newUserRecord = newUserRecord.sort((a, b) => {
+      if (!a.timeIn || !b.timeIn) {
+        return 1;
+      }
+      return new Date(b.timeIn) - new Date(a.timeIn);
+    })
+    return newUserRecord;
   },
   momentFormat: function(time) {
-    if ((moment().unix() - moment(time).unix()) < 3600) {
-      return moment(time).fromNow();
-    }else{
-      return moment(time).format("DD-MM-YYYY HH:mm");
-    };
+    return moment(time).format("HH:mm:ss");
   }
 });
 
