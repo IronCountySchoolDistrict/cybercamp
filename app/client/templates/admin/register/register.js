0502423712787e1a8ef21195a33060766a44b7d6
Basiczombie <issacgable@gmail.com>
2016-04-26 11:58:37 -0600

    Refactored registration logic

--- app/client/templates/admin/register/register.js
+++ app/client/templates/admin/register/register.js
@@ -8,35 +8,9 @@ Template.Register.events({
     var passwordVar = event.target.regPassword.value;
     var role = event.target.regRole.value;
 
-    var _createUserAsync = function(userVar, passwordVar) {
-      return new Promise((resolve, reject) => {
-        Accounts.createUser({
-          username: userVar,
-          password: passwordVar
-        },
-        function(error,result){
-          if(error){
-            console.log(error.reason);
-            reject(error.reason);
-          }else{
-            var userId = Meteor.userId();
-            resolve(userId);
-          }
-        })
+    Meteor.call('createGroup', userVar, passwordVar, role);
 
-      });
-    };
-
-    _createUserAsync(userVar, passwordVar)
-      .then(userId => {
-        if(role === 'admin'){
-          Meteor.call('adminRole', userId);
-        }else{
-          Meteor.call('defaultRole', userId);
-        }
-        Router.go('register');
-      });
-
+    event.target.reset();
     /*if(role === 'admin'){
       Roles.setUserRoles(userId, 'admin');
     }else{
--- app/server/methods.js
+++ app/server/methods.js
@@ -9,5 +9,35 @@ Meteor.methods({
 
   'defaultRole': function (userId) {
     Roles.setUserRoles(userId, 'student');
+  },
+  'createGroup': function (userVar, passwordVar, role){
+    var _createUserAsync = function(userVar, passwordVar) {
+      return new Promise((resolve, reject) => {
+        var user = Accounts.createUser({
+          username: userVar,
+          password: passwordVar
+        });
+        resolve(user);
+      })
+    }
+        /*function(error,result){
+          if(error){
+            console.log(error.reason);
+            reject(error.reason);
+          }else{
+            var userId = Meteor.userId();
+
+          }
+        })*/
+
+  _createUserAsync(userVar, passwordVar)
+    .then(userId => {
+      if(role === 'admin'){
+        Meteor.call('adminRole', userId);
+      }else{
+        Meteor.call('defaultRole', userId);
+      }
+      Router.go('register');
+    });
   }
 });
